765b89edbb4f66b2d005a3fb68382fde779d72ed
Riccardo Casatta <riccardo@casatta.it>
2025-01-18 18:13:01 +0100

    add simpler read numbers fn

--- src/number.rs
+++ src/number.rs
@@ -126,11 +126,92 @@ impl U16 {
     }
 }
 
+#[inline(always)]
+pub fn read_u8(slice: &[u8]) -> Result<u8, Error> {
+    slice.first().cloned().ok_or(Error::MoreBytesNeeded)
+}
+
+#[inline(always)]
+pub fn read_u16(slice: &[u8]) -> Result<u16, Error> {
+    let arr: [u8; 2] = slice
+        .get(..2)
+        .ok_or(Error::MoreBytesNeeded)?
+        .try_into()
+        .expect("slice length is 2");
+    Ok(u16::from_le_bytes(arr))
+}
+
+#[inline(always)]
+pub fn read_u32(slice: &[u8]) -> Result<u32, Error> {
+    let arr: [u8; 4] = slice
+        .get(..4)
+        .ok_or(Error::MoreBytesNeeded)?
+        .try_into()
+        .expect("slice length is 4");
+    Ok(u32::from_le_bytes(arr))
+}
+
+#[inline(always)]
+pub fn read_i32(slice: &[u8]) -> Result<i32, Error> {
+    let arr: [u8; 4] = slice
+        .get(..4)
+        .ok_or(Error::MoreBytesNeeded)?
+        .try_into()
+        .expect("slice length is 4");
+    Ok(i32::from_le_bytes(arr))
+}
+
+#[inline(always)]
+pub fn read_u64(slice: &[u8]) -> Result<u64, Error> {
+    let arr: [u8; 8] = slice
+        .get(..8)
+        .ok_or(Error::MoreBytesNeeded)?
+        .try_into()
+        .expect("slice length is 8");
+    Ok(u64::from_le_bytes(arr))
+}
+
 #[cfg(test)]
 mod test {
     use crate::ParseResult;
 
     use super::*;
+    #[test]
+    fn test_read_functions() {
+        assert_eq!(read_u8(&[1u8]), Ok(1u8));
+        assert_eq!(read_u8(&[255u8]), Ok(255u8));
+        assert_eq!(read_u8(&[1u8, 2]), Ok(1u8));
+        assert_eq!(read_u8(&[]), Err(Error::MoreBytesNeeded));
+
+        assert_eq!(read_u32(&[1u8, 0, 0, 0]), Ok(1u32));
+        assert_eq!(read_u32(&[0u8, 1, 0, 0]), Ok(256u32));
+        assert_eq!(read_u32(&[136u8, 19, 0, 0]), Ok(5000u32));
+        assert_eq!(read_u32(&[32u8, 161, 7, 0]), Ok(500000u32));
+        assert_eq!(read_u32(&[10u8, 10, 10, 10]), Ok(168430090u32));
+        assert_eq!(read_u32(&[1u8]), Err(Error::MoreBytesNeeded));
+        assert_eq!(read_u32(&[1u8, 2]), Err(Error::MoreBytesNeeded));
+        assert_eq!(read_u32(&[1u8, 2, 3]), Err(Error::MoreBytesNeeded));
+
+        assert_eq!(read_i32(&[255u8, 255, 255, 255]), Ok(-1i32));
+        assert_eq!(read_i32(&[0u8, 255, 255, 255]), Ok(-256i32));
+        assert_eq!(read_i32(&[120u8, 236, 255, 255]), Ok(-5000i32));
+        assert_eq!(read_i32(&[32u8, 161, 7, 0]), Ok(500000i32));
+        assert_eq!(read_i32(&[1u8]), Err(Error::MoreBytesNeeded));
+        assert_eq!(read_i32(&[1u8, 2]), Err(Error::MoreBytesNeeded));
+        assert_eq!(read_i32(&[1u8, 2, 3]), Err(Error::MoreBytesNeeded));
+
+        assert_eq!(read_u64(&[1u8, 0, 0, 0, 0, 0, 0, 0]), Ok(1u64));
+        assert_eq!(
+            read_u64(&[10u8, 10, 10, 10, 10, 10, 10, 10]),
+            Ok(723401728380766730u64)
+        );
+        assert_eq!(read_u64(&[1u8]), Err(Error::MoreBytesNeeded));
+        assert_eq!(read_u64(&[1u8, 2, 3, 4]), Err(Error::MoreBytesNeeded));
+        assert_eq!(
+            read_u64(&[1u8, 2, 3, 4, 5, 6, 7]),
+            Err(Error::MoreBytesNeeded)
+        );
+    }
 
     #[test]
     fn numbers() {
