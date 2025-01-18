603a28795a731a2427e968edd6228301e4762d59
Riccardo Casatta <riccardo@casatta.it>
2025-01-18 19:12:26 +0100

    avoid read_slice in OutPoint

--- src/bsl/out_point.rs
+++ src/bsl/out_point.rs
@@ -1,4 +1,4 @@
-use crate::{slice::read_slice, Parse, ParseResult, SResult};
+use crate::{Error, Parse, ParseResult, SResult};
 
 /// The out point of a transaction input, identifying the previous output being spent
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -15,13 +15,8 @@ impl<'a> AsRef<[u8]> for OutPoint<'a> {
 impl<'a> Parse<'a> for OutPoint<'a> {
     /// Parse the out point from the given slice
     fn parse(slice: &'a [u8]) -> SResult<Self> {
-        let outpoint = read_slice(slice, 36usize)?;
-        Ok(ParseResult::new(
-            outpoint.remaining(),
-            OutPoint {
-                slice: outpoint.parsed_owned(),
-            },
-        ))
+        let (slice, remaining) = slice.split_at_checked(36).ok_or(Error::MoreBytesNeeded)?;
+        Ok(ParseResult::new(remaining, OutPoint { slice }))
     }
 }
 impl<'a> OutPoint<'a> {
