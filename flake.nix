6e0e2702ef6f4178f7f137ab26274ecdd9a4521d
Riccardo Casatta <riccardo@casatta.it>
2026-03-20 18:50:05 +0100

    add flake.nix and rust-toolchain.toml

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Bitcoin slices - zero allocation Bitcoin parsing library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        # Read the rust-toolchain.toml to get the exact version
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+          ];
+
+          # Optionally add additional development tools
+          nativeBuildInputs = with pkgs; [
+            # For criterion benchmarks
+            gnuplot
+          ];
+        };
+      }
+    );
+}
